74489f7c5960af8c139ee4d308bd6a60e279d9bd
NicoFara <nicolas.fara.romero@gmail.com>
2022-07-17 10:57:59 -0300

    Ejercicio 15 finalizado

---
+++ js/ej15.js
@@ -0,0 +1,14 @@
+// 15- Realiza un script que cuente el número de vocales que tiene un texto.
+
+
+let texto = prompt("Ingrese su texto");
+let minus = texto.toLowerCase();
+let vocales = 0;
+
+for(let i=0; i<=minus.length; i++) {
+    if(minus.charAt(i) === "a" || minus.charAt(i) === "e" || minus.charAt(i) === "i" || minus.charAt(i) === "o" ||minus.charAt(i) === "u") {
+        vocales++;
+    }
+}
+
+document.write(`Hay ${vocales} vocales en el texto`);
